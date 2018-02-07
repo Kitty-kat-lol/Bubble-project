0fe6c03b002e8b4fbd795d530ed9980b3a047329
Kitty-kat-lol <32311727+Kitty-kat-lol@users.noreply.github.com>
2018-02-07 10:13:22 -0500

    creation init classes

---
+++ Bulle.h
@@ -0,0 +1,20 @@
+//
+//
+#ifendefF BULLE_H
+#define BULLE_H
+
+#include "Coordonne.h"
+#include <iostream>
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ Coordonne.h
@@ -0,0 +1,21 @@
+#ifendefF COORDONNE_H
+#define COORDONNE_H
+
+
+
+
+struct Coordonnee{
+	int x;
+	int y;
+};
+
+
+
+
+
+
+
+
+
+
+#endif
